85ddd194bc1b61c64653e6b2b87b62462ae0bdfb
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-08-03 23:10:32 +0530

    basic frontend complete

---
+++ script/script.js
@@ -0,0 +1,46 @@
+let query = "";
+$(document).ready(() => {
+    $('#iConsent').removeAttr('checked');
+    $('.qry').removeAttr('checked');
+});
+
+$('.query-type').click(function () {
+    $('.qry').prop('checked', false);
+    const classes = $(this).attr('class');
+    if (classes.includes('query-1')) {
+        $(this).addClass('highlight');
+        $('#query1').prop('checked', true);
+        $('.query-2').removeClass('highlight');
+        query = $('#query1').val();
+        return;
+    }
+    $(this).addClass('highlight');
+    $('#query2').prop('checked', true);
+    $('.query-1').removeClass('highlight');
+    query = $('#query2').val();
+})
+
+$('#iConsent').change(function () {
+    $('.submit-button input').toggleClass('submit-form');
+});
+
+$('.submit-form').click(() => {
+    if (checkForm) {
+        alert("success");
+    }
+    return
+});
+
+function checkForm() {
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const email = $('#email').val();
+    const queryType = query;
+    const message = $('#message').val();
+    
+    if (firstName || lastName || email || queryType || message) {
+        alert('not empty');
+    } else {
+        alert('empty');
+    }
+}
