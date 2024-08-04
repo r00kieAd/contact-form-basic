59e834bd928d6a5b4e764f12ab076a7e8183c1da
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-08-05 02:13:47 +0530

    backend initialized with async issues

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import path, { dirname } from 'path';
 import { fileURLToPath } from "url";
+import { promises as fs } from 'fs';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
@@ -11,10 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 // a custom middleware to log request and responses
-function customLogger(req, res, next) {
-    console.log(`Request Method: ${req.method}, requested url: ${req.url}, request status: ${req.status}`);
-    next();
-}
 app.use(customLogger);
 
 app.get('/index.html', (req, res) => {
@@ -22,10 +19,59 @@ app.get('/index.html', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post('/success.html', (req, res) => {
-    res.status(404);
-    res.send('<h1>Error 404</h1>')
-})
+app.post('/success.html', async (req, res) => {
+    const obj = req.body;
+    const state = await updateJson(obj);
+    try {
+        if (state.code != 200) {
+            res.status(state.code).json({ error: state.msg });
+            return;
+        }
+        res.status(200);
+        res.json({ redirectUrl: '/success.html' });
+    } catch (err) {
+        console.log(state);
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+});
+
+async function updateJson(obj) {
+    try {
+      const data = await fs.readFile("data/data.json", "utf8");
+  
+      let jsonArray = [];
+  
+      if (data) {
+        try {
+          jsonArray = JSON.parse(data);
+          if (!Array.isArray(jsonArray)) {
+            jsonArray = [];
+          }
+        } catch (parseErr) {
+          console.log("Error parsing JSON:", parseErr);
+          return { code: 500, msg: "Internal Server Error" };
+        }
+      }
+  
+      jsonArray.push(obj);
+  
+      // formatting with 2 spaces at the time of stringifying
+      const updateData = JSON.stringify(jsonArray, null, 2);
+  
+      await fs.writeFile("data/data.json", updateData);
+      console.log("File updated");
+  
+      return { code: 200, msg: "success" };
+    } catch (err) {
+      console.log(err);
+      return { code: 500, msg: "Internal Server Error" };
+    }
+  }
+
+function customLogger(req, res, next) {
+    console.log(`Request Method: ${req.method}, requested url: ${req.url}, request status: ${res.status}`);
+    next();
+}
 
 app.listen(port, () => {
     console.log(`This server is running on port ${port}. Link: 'http://localhost:${port}/index.html'`);
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -44,10 +44,10 @@ $('#iConsent').change(function () {
 $('#formMain').submit((event) => {
     event.preventDefault();
     if (!submit) return;
-    valid = checkForm(getValues());
+    const form_data = getValues();
+    valid = checkForm(form_data);
     if (valid) {
-        alert('form submitted');
-        return;
+        submitForm(form_data);
     } else {
         submit = false;
         $('#iConsent').prop('checked', false);
@@ -55,55 +55,91 @@ $('#formMain').submit((event) => {
     }
 });
 
-function getValues() {
-    const firstName = $('#firstname').val();
-    const lastName = $('#lastname').val();
-    const email = $('#email').val();
-    const queryType = query;
-    const message = $('#message').val();
-    return [firstName, lastName, email, queryType, message];
+async function submitForm(form_data) {
+    let result = await $.ajax({
+        url: "/success.html",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            firstName: form_data[0], lastName: form_data[1],
+            email: form_data[2], queryType: form_data[3],
+            message: form_data[4]
+        })
+
+        // success: function (result) {
+        //     if (result.redirectUrl) {
+        //         window.location.href = result.redirectUrl;
+        //     }
+        // },
+        // error: function(xhr) {
+        //     if (xhr.status !=  200) {
+        //         $('#formMain').after(`<p class='serverResponse'>Status ${xhr.status}: ${xhr.statusText}</p>`);
+        //     } else {
+        //         alert(`unknown error: ${xhr.status}`);
+        //     }
+        // }
+    });
+    if (result.redirectUrl) {
+        window.location.href = result.redirectUrl;
+    }
 }
 
-function checkForm(vars) {
+    function getValues() {
+        const firstName = $('#firstname').val();
+        const lastName = $('#lastname').val();
+        const email = $('#email').val();
+        const queryType = query;
+        const message = $('#message').val();
+        return [firstName, lastName, email, queryType, message];
+    }
 
-    if (!valid) return false;
+    function checkForm(vars) {
 
-    const nameRegex = /[0-9]|[!@#$%^&*()]/g;
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!valid) return false;
 
-    if (vars[0]) {
-        if (nameRegex.test(vars[0])) {
-            $('#firstname').after('<p class="err">name should only contain alphabets</p>');
+        const nameRegex = /[0-9]|[!@#$%^&*()]/g;
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (vars[0]) {
+            if (nameRegex.test(vars[0])) {
+                $('#firstname').after('<p class="err">name should only contain alphabets</p>');
+                $('#firstname').addClass('errinpt');
+                valid = false;
+            }
+        } else {
+            $('#firstname').after('<p class="err">name should not be empty</p>');
             $('#firstname').addClass('errinpt');
+            valid = false;
         }
-    } else {
-        $('#firstname').after('<p class="err">name should not be empty</p>');
-        $('#firstname').addClass('errinpt');
-    }
 
-    if (vars[1]) {
-        if (nameRegex.test(vars[1])) {
-            $('#lastname').after('<p class="err">name should only contain alphabets</p>');
+        if (vars[1]) {
+            if (nameRegex.test(vars[1])) {
+                $('#lastname').after('<p class="err">name should only contain alphabets</p>');
+                $('#lastname').addClass('errinpt');
+                valid = false;
+            }
+        } else {
+            $('#lastname').after('<p class="err">name should not be empty</p>');
             $('#lastname').addClass('errinpt');
+            valid = false;
         }
-    } else {
-        $('#lastname').after('<p class="err">name should not be empty</p>');
-        $('#lastname').addClass('errinpt');
-    }
 
-    if (!emailRegex.test(vars[2])) {
-        $('#email').after('<p class="err">invalid email</p>');
-        $('#email').addClass('errinpt');
-    }
+        if (!emailRegex.test(vars[2])) {
+            $('#email').after('<p class="err">invalid email</p>');
+            $('#email').addClass('errinpt');
+            valid = false;
+        }
 
-    if (!vars[3]) {
-        $('#queriesDiv').after('<p class="err">select a query type</p>');
-        $('.query-type').addClass('errinpt');
-    }
+        if (!vars[3]) {
+            $('#queriesDiv').after('<p class="err">select a query type</p>');
+            $('.query-type').addClass('errinpt');
+            valid = false;
+        }
 
-    if (!vars[4]) {
-        $('.message-div').after('<p class="err">a message is required</p>');
-        $('#message').addClass('errinpt');
+        if (!vars[4]) {
+            $('.message-div').after('<p class="err">a message is required</p>');
+            $('#message').addClass('errinpt');
+            valid = false;
+        }
+        return valid;
     }
-    return false;
-}
